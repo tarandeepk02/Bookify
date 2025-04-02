413ff458fc220bcc59e86f6d97f2537ab52c0a0a
Tarandeep Kaur <taranbenipal02@gmail.com>
2025-04-02 12:35:59 -0700

    Milestone 1

---
+++ models/config/config.js
@@ -0,0 +1,8 @@
+(() => {
+    const config = {}
+    config.SERVER = 'summer.z7cjv.mongodb.net'
+    config.USERNAME = 'taranbenipal02summer'
+    config.PASSOWRD = 'IPCUFp8nEgNb25Uc'
+    config.DATABASE = 'Forum'
+    module.exports = config
+})()
