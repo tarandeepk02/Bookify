b31311d5abfe4f8acf6afe02dd2b2a0000468d29
Jivanjot Singh <jsingh346@mylangara.ca>
2025-04-09 23:22:34 -0700

    Checkout Bugs Fixed

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -27,16 +27,16 @@ checkoutController.post('/checkout', util.logRequest, orderValidationRules, asyn
     }
     const userId = req.session && req.session.user ? req.session.user.id : ''
     const userRole = req.session && req.session.user ? 'member' : 'guest'
-    const { checkoutName, checkoutEmail, shippingAddress, subtotal, shipping, total, cart,since } = req.body
+    const { checkoutName, checkoutEmail, shippingAddress, subtotal, shipping, total, cart, since } = req.body
 
     try {
         if (userRole == 'guest') {
             let collectionUser = client.db().collection('Users')
-            let user = { name: checkoutName, email: checkoutEmail, role: "guest",since }
+            let user = { name: checkoutName, email: checkoutEmail, role: "guest", since }
             await util.insertOne(collectionUser, user)
         }
         let collection = client.db().collection('Orders')
-        let order = { userId, userRole, checkoutName, checkoutEmail, shippingAddress, subtotal, shipping, total, cart,since }
+        let order = { userId, userRole, checkoutName, checkoutEmail, shippingAddress, subtotal, shipping, total, cart, since }
 
         await util.insertOne(collection, order)
 
@@ -47,7 +47,7 @@ checkoutController.post('/checkout', util.logRequest, orderValidationRules, asyn
     }
 })
 
-checkoutController.get('/orders',util.authenticateUser, async (req, res, next) => {
+checkoutController.get('/orders', util.authenticateUser, async (req, res, next) => {
     try {
         const userId = req.session?.user?.id
         if (!userId) {
@@ -82,7 +82,7 @@ checkoutController.get('/orders/all', util.authenticateUser, util.authenticateRo
 })
 
 
-checkoutController.get('/orders/:id',util.authenticateUser, async (req, res, next) => {
+checkoutController.get('/orders/:id', util.authenticateUser, async (req, res, next) => {
     try {
         const orderId = req.params.id
         if (!ObjectId.isValid(orderId)) {
@@ -106,7 +106,7 @@ checkoutController.get('/orders/:id',util.authenticateUser, async (req, res, nex
     }
 })
 
-checkoutController.get('/queries',util.authenticateUser, util.authenticateRole('admin'), async (req, res, next) => {
+checkoutController.get('/queries', util.authenticateUser, util.authenticateRole('admin'), async (req, res, next) => {
     try {
 
         if (!req.session?.user || req.session.user.role !== 'admin') {
